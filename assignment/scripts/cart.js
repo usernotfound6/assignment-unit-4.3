86f77b63b6980f2ba0ad57cc7fb65ce741b87d9b
Fraol Ayehu <phrolaye@gmail.com>
2023-08-01 17:55:14 -0500

    changed var to let

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,7 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-var basket = [];
+let basket = [];
 
 function addItem(item) {
     basket.push(item);
